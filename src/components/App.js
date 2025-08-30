1895d8c4315fd57c26af77bae75ca62cec823b0a
Basant-spec <basantdhiman9@gmail.com>
2025-08-30 12:40:23 +0530

    COMPLETED ASSIGNMENT

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useEffect, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    // Fetch data asynchronously
+    fetch("https://dummyjson.com/products")
+      .then((res) => res.json())
+      .then((json) => setData(json))
+      .catch((err) => console.error("Error fetching data:", err));
+  }, []);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Data Fetcher</h1>
+      {/* Display JSON inside <pre> */}
+      <pre>{data ? JSON.stringify(data, null, 2) : "Loading..."}</pre>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
